071a02a8c34cd8ce734e498eba9c4914e1994435
Don L <Don L>
2016-11-13 02:15:07 +0800

    added name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+/*$("#main").append("LiDY");*/
+
+var formattedName = HTMLheaderName.replace("%data%", "LiDY");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+ 
